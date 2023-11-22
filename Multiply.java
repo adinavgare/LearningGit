c694cff2096c53c15b9e0e5f09b59226de860bc5
Aditya Shivkumar Navgare <adityanavgare162@gmail.com>
2023-11-22 13:50:00 +0530

    Addded multiplyyy file

--- Multiply.java
+++ Multiply.java
@@ -3,7 +3,7 @@ public class Multiply
     public static void main(String[] args)
     {
         int a=5, b=4;
-        System.out.println("The Multiplication is: " + ab);
+        System.out.println("The Multiplication is: " + a*b);
 
     }
 }
